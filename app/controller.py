12af5bb4586226df659937d454f82dfaf3e146c0
cdr87 <cdr87@rutgers.edu>
2015-09-26 19:00:40 -0400

    some new methods

--- app/controller.py
+++ app/controller.py
@@ -2,6 +2,23 @@ from . import app, clarifai_api
 from flask import render_template
 from pprint import pprint
 
+from instagram.client import InstagramAPI
+
+access_token = "e028ff7d074940f087f6248c9df7b3a9"
+client_secret = "c8450b5e4dc54ef4afe2b0040a1ee546"
+insta = InstagramAPI(access_token=e028ff7d074940f087f6248c9df7b3a9, client_secret=c8450b5e4dc54ef4afe2b0040a1ee546)
+
+def get_user_insta_liked_media(userID="user_id"):
+	"""
+    Returns a list of all the tags for a photo using clarifai's
+    image recognition.
+    """
+	recent_likes, next_ = api.user_recent_media(user_id, count, max_id)
+	liked_media = []
+	for like in recent_likes:
+	   liked_media.append('<img src="%s"/>' % media.images['thumbnail'].url)
+	return liked_media
+
 def get_tags_for_photo(photo_url):
     """
     Returns a list of all the tags for a photo using clarifai's
