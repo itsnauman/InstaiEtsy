460b7b46edd278d56601716be1cee46b08ce6c38
cdr87 <cdr87@rutgers.edu>
2015-09-26 16:30:03 -0400

    added clarifai creds

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,6 @@ from flask import Flask
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = 'secret_key'
-
+app.config['uOgH1zIg0WN1gDWHyevACltptRBPGttljs6VwuTI'] = '18p3NWU1Z_e5paBLLL8bk50ZPMWoOuk4U7cgR3Wj'
+#access token: E1R748x3rEp3mEr5F6rQIBEHPFf43w
 from . import controller
