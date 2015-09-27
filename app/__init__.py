9d964e883e513ec49b05af52508607876734f3f8
Nauman Ahmad <nauman-ahmad@outlook.com>
2015-09-26 20:47:34 -0400

    Etsy api method implemented

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,14 @@
 from flask import Flask
 from clarifai.client import ClarifaiApi
+from instagram.client import InstagramAPI
 
 app = Flask(__name__)
-clarifai_api = ClarifaiApi(app_id="EnSVY5w5DxxvH2Fq-XrkHbdtge5favWmTNybItti", app_secret="TFlBASaMNs8S4VzRsxXQUQeMPoW46T9KbGXkzD3V")
+
+access_token = "e028ff7d074940f087f6248c9df7b3a9"
+client_secret = "c8450b5e4dc54ef4afe2b0040a1ee546"
 ETSY_API_KEY = "g3kguayhagkdmstc7f6hnaa2"
+
+clarifai_api = ClarifaiApi(app_id="EnSVY5w5DxxvH2Fq-XrkHbdtge5favWmTNybItti", app_secret="TFlBASaMNs8S4VzRsxXQUQeMPoW46T9KbGXkzD3V")
+insta_api = InstagramAPI(access_token=access_token, client_secret=client_secret)
 
 from . import controller
--- app/controller.py
+++ app/controller.py
@@ -1,14 +1,9 @@
-from . import app, clarifai_api, ETSY_API_KEY
+from . import app, clarifai_api, ETSY_API_KEY, insta_api
 from flask import render_template
 import requests
 from pprint import pprint
-from instagram.client import InstagramAPI
 
 user_tags = {}
-
-access_token = "e028ff7d074940f087f6248c9df7b3a9"
-client_secret = "c8450b5e4dc54ef4afe2b0040a1ee546"
-insta_api = InstagramAPI(access_token=e028ff7d074940f087f6248c9df7b3a9, client_secret=c8450b5e4dc54ef4afe2b0040a1ee546)
 
 def get_user_insta_liked_media(USERID="user_id", COUNT=10, MAX_ID="max_id"):
 	"""
@@ -29,14 +24,6 @@ def get_tags_for_photo(photo_url):
     tag_list = result['results'][0]['result']['tag']['classes']
     return tag_list
 
-<<<<<<< HEAD
-def get_etsy_results_simple(tag_list):
-	"""
-    Returns a list of Etsy product listings based on image recognition results.
-    """
-    for tag in tag_list:
-    	#do something with Etsy API
-
 def update_user_tags_and_recs(tag_list):
 	for tag in tag_list:
 		user_tags[tag] = 0
@@ -47,31 +34,28 @@ def update_rankings(user_tags, tag_list):
 			if key==list_tag:
 				++user_recs[key]
 
-def get_etsy_results_ml(user_tags, tag_list):
-	"""
-    Returns a list of Etsy product listings based on image recognition results.
+# def get_etsy_results_ml(user_tags, tag_list):
+# 	"""
+#     Returns a list of Etsy product listings based on image recognition results.
+#     """
+# 	#sorted by values (rank)
+# 	sorted_user_tags = sorted(user_tags.items(), key=operator.itemgetter(1))
+
+# 	for key, val in user_tags:
+# 		for list_tag in tag_list:
+# 			if (key==list_tag) &&:
+
+#     	#do something with Etsy API
+
+def get_etsy_products(tag_list):
     """
-
+    Returns a list of all the matching items currenly on sale at Etsy
     """
-    values = list(d.values())
-	max_rank = max(values)
-	max_rank_index = values.index(max_value)
-	"""
-	#sorted by values (rank)
-	sorted_user_tags = sorted(user_tags.items(), key=operator.itemgetter(1))
-
-	for key, val in user_tags:
-		for list_tag in tag_list:
-			if (key==list_tag) &&:
-
-    	#do something with Etsy API
-
-=======
-def get_etsy_products():
-    payload = {'api_key': ETSY_API_KEY}
-    res = requests.get('')
->>>>>>> 1fabec0d75ac7c0297233d9d0950713fdfe2288a
+    payload = {'api_key': ETSY_API_KEY, 'keywords': tag_list}
+    res = requests.get('https://openapi.etsy.com/v2/listings/active', params=payload)
+    return res.text
 
 @app.route('/')
 def index():
+    print get_etsy_products()
     return "Hello World"
