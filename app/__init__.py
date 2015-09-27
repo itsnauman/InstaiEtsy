58a8d44a41dab3dc2299f229e572ecfec476df9f
Nauman Ahmad <nauman-ahmad@outlook.com>
2015-09-27 00:39:59 -0400

    Instagram API implementation

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,17 @@
 from flask import Flask
 from clarifai.client import ClarifaiApi
-from instagram.client import InstagramAPI
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+Bootstrap(app)
 
-access_token = "e028ff7d074940f087f6248c9df7b3a9"
-client_secret = "c8450b5e4dc54ef4afe2b0040a1ee546"
+app.secret_key = 'very_secret_key'
+# Instagram API Details
+access_token = "5d99f0fac1084de5a71f889452248123"
+client_secret = "aba8aa200eaf417ca9ef54a9b83eddfd"
+
 ETSY_API_KEY = "g3kguayhagkdmstc7f6hnaa2"
 
 clarifai_api = ClarifaiApi(app_id="EnSVY5w5DxxvH2Fq-XrkHbdtge5favWmTNybItti", app_secret="TFlBASaMNs8S4VzRsxXQUQeMPoW46T9KbGXkzD3V")
-insta_api = InstagramAPI(access_token=access_token, client_secret=client_secret)
 
 from . import controller
